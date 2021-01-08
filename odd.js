2494b9e1196030325a2e63683e61e4b758e0560b
Adam Mohammed <13.adamm@gmail.com>
2021-01-07 22:21:04 -0500

    Odd Checker assignment

---
+++ odd.js
@@ -0,0 +1,15 @@
+
+function isOdd(num) {
+	if (num % 2 == 0)
+		return false;
+	else
+		return true;
+}
+
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log("13 is odd: " + isOdd(13));
+console.log("8423 is odd: " + isOdd(8423));
+console.log("2343 is odd: " + isOdd(2343));
+console.log("8654 is odd: " + isOdd(8654));
